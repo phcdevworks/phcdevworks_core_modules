7b75c1402174ccc0179935451200e54ede20f782
BradPotts <brad.potts@phcnet.co>
2020-06-29 13:09:55 +0000

    updated class for route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ PhcdevworksCoreModules::Engine.routes.draw do
 
   # Core Module Routes
   namespace :modules do
-    resources :categories
+    resources :categories, class_name: 'Core::Category'
   end
 
   # Mount Routes
