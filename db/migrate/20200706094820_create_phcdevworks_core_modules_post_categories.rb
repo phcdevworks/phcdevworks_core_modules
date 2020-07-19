e251f2a554e08bbd555dcbc55ccd0b842b8b20cd
BradPotts <brad.potts@phcnet.co>
2020-07-19 09:23:33 +0000

    minor db ording change won't affect function.

--- db/migrate/20200706094820_create_phcdevworks_core_modules_post_categories.rb
+++ db/migrate/20200706094820_create_phcdevworks_core_modules_post_categories.rb
@@ -4,11 +4,11 @@ class CreatePhcdevworksCoreModulesPostCategories < ActiveRecord::Migration[6.0]
 
       t.string :post_category_name
 
+      t.string :optimization_id
+
       t.string :slug
       t.string :user_id
       t.string :org_id
-
-      t.string :optimization_id
 
       t.timestamps
 
