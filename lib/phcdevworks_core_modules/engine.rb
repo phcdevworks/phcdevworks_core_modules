614d40ca40d4de25e633b6ac06d7749743c0c036
BradPotts <brad.potts@phcnet.co>
2020-12-01 10:11:14 +0000

    removed default rails test unit.

--- lib/phcdevworks_core_modules/engine.rb
+++ lib/phcdevworks_core_modules/engine.rb
@@ -26,6 +26,9 @@ module PhcdevworksCoreModules
     require "gravtastic"
     require "friendly_id"
 
+    # Mailer Dependencies
+    require "mail_form"
+
     # Load User Accounts
     require "phcdevworks_accounts"
 
